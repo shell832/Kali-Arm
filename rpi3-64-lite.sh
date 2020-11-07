d876c65cd85b5c0cbeaaeec0a228c2ad5ab59e1e
Steev Klimaszewski <steev@kali.org>
2020-11-07 09:34:06 -0600

    crda: Add to the two scripts that don't install network-manager
    Normally crda is pulled in as a recommends from network-manager but not all scripts install network-manager, so lets install it manually since we try to edit the default config for it in the scripts, which causes the script to fail if the file can't be edited.

--- rpi3-64-lite.sh
+++ rpi3-64-lite.sh
@@ -80,7 +80,8 @@ arm="fake-hwclock ntpdate u-boot-tools"
 base="apt-transport-https apt-utils console-setup e2fsprogs firmware-linux firmware-realtek firmware-atheros firmware-libertas ifupdown initramfs-tools iw kali-defaults man-db mlocate netcat-traditional net-tools parted pciutils psmisc rfkill screen snmpd snmp sudo tftp tmux unrar usbutils vim wget zerofree"
 tools="aircrack-ng crunch cewl dnsrecon dnsutils ethtool exploitdb hydra john libnfc-bin medusa metasploit-framework mfoc ncrack nmap passing-the-hash proxychains recon-ng sqlmap tcpdump theharvester tor tshark usbutils whois windows-binaries winexe wpscan wireshark"
 services="apache2 atftpd openssh-server openvpn tightvncserver"
-extras="bluez bluez-firmware i2c-tools python3-configobj python3-pip python3-requests python3-rpi.gpio python3-smbus triggerhappy wpasupplicant"
+extras="bluez bluez-firmware crda i2c-tools python3-configobj python3-pip python3-requests python3-rpi.gpio python3-smbus triggerhappy wpasupplicant"
+
 
 packages="${arm} ${base} ${services}"
 
