907c24083b7b3196c69eca6a85322c8973b406ee
Steev Klimaszewski <steev@kali.org>
2020-12-08 14:11:12 -0600

    pinebook-pro: And remove the last patch we don't need

--- pinebook-pro.sh
+++ pinebook-pro.sh
@@ -341,7 +341,7 @@ cd linux
 touch .scmversion
 #patch -p1 --no-backup-if-mismatch < ${current_dir}/patches/pinebook-pro/0001-net-smsc95xx-Allow-mac-address-to-be-set-as-a-parame.patch
 #patch -p1 --no-backup-if-mismatch < ${current_dir}/patches/pinebook-pro/0008-board-rockpi4-dts-upper-port-host.patch
-patch -p1 --no-backup-if-mismatch < ${current_dir}/patches/pinebook-pro/0008-rk-hwacc-drm.patch
+#patch -p1 --no-backup-if-mismatch < ${current_dir}/patches/pinebook-pro/0008-rk-hwacc-drm.patch
 patch -p1 --no-backup-if-mismatch < ${current_dir}/patches/pinebook-pro/kali-wifi-injection.patch
 cp ${current_dir}/kernel-configs/pinebook-pro-5.7.config .config
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- oldconfig
